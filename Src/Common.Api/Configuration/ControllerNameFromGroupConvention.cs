24da6aef2cf505521929eb2e1b15e94896c57106
William Pei <william.h.pei@gmail.com>
2021-04-17 15:28:53 +1000

    feat: adds common api and application projects

---
+++ Src/Common.Api/Configuration/ControllerNameFromGroupConvention.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Common.Api.Configuration
+{
+    public class ControllerNameFromGroupConvention : IControllerModelConvention
+    {
+        /// <inheritdoc />
+        public void Apply(ControllerModel controller)
+        {
+            if (string.IsNullOrWhiteSpace(controller.ApiExplorer.GroupName)) return;
+
+            controller.ControllerName = controller.ApiExplorer.GroupName;
+        }
+    }
+}
---
+++ Src/Common.Application/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+using Common.Application.Behaviours;
+using Common.Application.Mappings;
+
+using FluentValidation;
+
+using MediatR;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Application
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Adds AutoMapper profile, Fluent Validators, and MediatR Requests and Handlers
+        /// </summary>
+        /// <param name="services">The current <see cref="IServiceCollection"/></param>
+        public static void AddCommonApplication(this IServiceCollection services)
+        {
+            var callingAssembly = Assembly.GetCallingAssembly();
+            services.AddAutoMapper(config =>
+            {
+                config.AddProfile(new MappingProfile(callingAssembly));
+            });
+            services.AddValidatorsFromAssembly(callingAssembly);
+            services.AddMediatR(callingAssembly);
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        }
+    }
+}
